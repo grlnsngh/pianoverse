d4820b53f77e4c1a27519b46ab8942e760dfcea2
Gurleen <grlnsngh@gmail.com>
2024-08-08 17:07:35 -0400

    refactor: Update piano item types to include null values for rental_price and rental_period_end

--- redux/pianos/types.ts
+++ redux/pianos/types.ts
@@ -27,7 +27,7 @@ export interface PianoItem {
   rental_customer_name?: string | null;
   rental_period_end?: Date | null;
   rental_period_start?: Date | null;
-  rental_price?: number;
+  rental_price?: number | null;
   sold_date?: Date | null;
   sold_price?: string | null;
   sold_to_address?: string | null;
@@ -35,4 +35,33 @@ export interface PianoItem {
   title: string;
   users: User;
   warehouse_since_date?: Date | null;
+}
+
+export interface PianoItemFormStateType {
+  category: string;
+  creator: string;
+  description?: string;
+  event_b_number?: string | null;
+  event_company_associated?: string | null;
+  event_model_number?: string | null;
+  event_purchase_from?: string | null;
+  event_purchase_price?: number | null;
+  image_url: string;
+  make: string;
+  on_sale_import_date?: string | null; // Changed to string
+  on_sale_price?: number | null;
+  on_sale_purchase_from?: string | null;
+  rental_customer_address?: string | null;
+  rental_customer_mobile?: string | null;
+  rental_customer_name?: string | null;
+  rental_period_end?: string | null; // Changed to string
+  rental_period_start?: string | null; // Changed to string
+  rental_price?: number | null;
+  sold_date?: string | null; // Changed to string
+  sold_price?: string | null;
+  sold_to_address?: string | null;
+  sold_to_name?: string | null;
+  title: string;
+  users: User;
+  warehouse_since_date?: string | null; // Changed to string
 }
