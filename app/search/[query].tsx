1e34cf2b2e308dd49c0024e4756700ab1d99ebdc
Gurleen <grlnsngh@gmail.com>
2024-08-08 00:56:08 -0400

    refactor: Update search page to display query in navigation title

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,10 +1,16 @@
-import { useLocalSearchParams } from "expo-router";
-import React from "react";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import React, { useEffect } from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ title: `Search ${query}` });
+  }, [query]);
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <Text className="text-3xl text-white">{query}</Text>
