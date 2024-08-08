4d28ef9a778cd99ea2afaf06b8c2437915e64ad7
Gurleen <grlnsngh@gmail.com>
2024-08-08 01:38:34 -0400

    refactor: Update search page to display query in navigation title

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,19 +1,57 @@
+import { PianoItem } from "@/redux/pianos/types";
+import { RootState } from "@/redux/store";
+import { searchPianoItems } from "@/utils/ObjectManipulation";
 import { useLocalSearchParams, useNavigation } from "expo-router";
-import React, { useEffect } from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector } from "react-redux";
+import EmptyState from "../components/EmptyState";
+import ListCard from "../components/ListCard";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
   const navigation = useNavigation();
+  const pianoItems = useSelector((state: RootState) => state.pianos.items);
+  const [items, setItems] = useState<PianoItem[]>([]);
 
   useEffect(() => {
-    navigation.setOptions({ title: `Search ${query}` });
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: "#FFA001",
+      },
+      headerTintColor: "#161622",
+      title: `Search results for ${query}`,
+    });
+    const searchResults = searchPianoItems(pianoItems, query[0]);
+    setItems(searchResults);
   }, [query]);
+
+  const [visibleMenuId, setVisibleMenuId] = useState<string | null>(null);
+
+  const openMenu = (menuId: string) => setVisibleMenuId(menuId);
+  const closeMenu = () => setVisibleMenuId(null);
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      <Text className="text-3xl text-white">{query}</Text>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <ListCard
+            item={item}
+            visibleMenuId={visibleMenuId}
+            openMenu={openMenu}
+            closeMenu={closeMenu}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Pianos Found"
+            subtitle="Try other search terms"
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
