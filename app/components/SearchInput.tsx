64e6ca36768adcc56fd80bee758f5a401cd1f297
Gurleen <grlnsngh@gmail.com>
2024-08-09 13:20:03 -0400

    refactor: Reorder SearchInput component structure and fix styling issues

--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -15,16 +15,9 @@ const SearchInput: React.FC<SearchInputProps> = ({ initialQuery }) => {
   return (
     <View
       style={{ height: 60 }}
-      className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary"
+      className="flex flex-row items-center space-x-4 w-full h-16 
+      px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary"
     >
-      <TextInput
-        className="text-base mt-0.5 text-white flex-1 font-pregular"
-        value={query}
-        placeholder="Search"
-        placeholderTextColor="#CDCDE0"
-        onChangeText={(e) => setQuery(e)}
-      />
-
       <TouchableOpacity
         onPress={() => {
           if (query === "")
@@ -39,6 +32,13 @@ const SearchInput: React.FC<SearchInputProps> = ({ initialQuery }) => {
       >
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Search"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
     </View>
   );
 };
