660a486a9d8901994897cf5b544e8e67b8132486
Gurleen <grlnsngh@gmail.com>
2024-08-13 01:10:15 -0400

    refactor: Update colors.ts to include category colors

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,2 +1,9 @@
 export const PRIMARY_COLOR = "#161622";
 export const SECONDARY_COLOR = "#FFA001";
+
+export const CATEGORY_COLORS = {
+  RENTABLE: "#efeaab",
+  EVENTS: "#abd8ef",
+  ON_SALE: "#eebec0",
+  WAREHOUSE: "#c0eebe",
+};
